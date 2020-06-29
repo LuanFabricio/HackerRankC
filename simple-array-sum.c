8f4d3bf2860c6ec727dfa6f03175c1edf0265fae
LuanCode <luanlimaleite@hotmail.com>
2020-06-29 18:06:33 -0300

    adicionando o simple-array-sum.c

---
+++ simple-array-sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int simpleArraySum(int ar_count, int* ar) {
+    int resultado = 0;
+    /*
+     * Write your code here.
+     */
+    for(int i = 0 ; i < ar_count ; i++)
+        resultado += ar[i];
+    return resultado;
+}
+int main (){
+    int arr[] = {1, 2, 3};
+    printf("\n%i\n",simpleArraySum(3, arr));
+    return 0;
+}
