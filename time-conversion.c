4295b4942a47dc6813c0ba13d6b44cb6a5737e58
LuanCode <luanlimaleite@hotmail.com>
2020-07-05 11:05:49 -0300

    time-conversion resolvido

--- time-conversion.c
+++ time-conversion.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 
 char* timeConversion(char* s) {
-    if(s[9] == 'A')
-        return s;
+    static char hora[8];
+    strncpy(hora, s, 8);
+    if(s[8] == 'A'){
+        if(hora[0] == '1' && hora[1] == '2'){
+            hora[0] = hora[1] = '0';
+        }
+        return hora;
+    }
     else
     {
-        printf("%c%c", (int)s[0]+1,(int)s[1]+2);
+        if(hora[0] == '1' && hora[1] == '2')
+            return hora;
+        int time[] = {(int)s[0] - 47, (int)s[1] - 46};
+        if(time[1] > 9){
+            time[0]++;
+            time[1] -= 10; 
+        }
+        hora[0] = (char)(time[0]+48);
+        hora[1] = (char)(time[1]+48);
+        return hora;
     }
     
 }
 
 int main(){
-    char hora[] = "07:05:45PM";
+    char hora[] = "12:05:45PM";
 
-    timeConversion(hora);
+    printf("%s", timeConversion(hora));
     return 0;
 }
