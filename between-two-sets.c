6c6c953754572bb551e0af983cd20fb31130bd08
= <luanlimaleite@hotmail.com>
2020-07-12 03:50:55 -0300

    quase terminando o between-two-sets

--- between-two-sets.c
+++ between-two-sets.c
@@ -1,47 +1,34 @@
 #include <stdio.h>
 
 int getTotalX(int a_count, int* a, int b_count, int* b) {
-    if(b_count == 1)
-        return 0;
-    
-    int i, j, div, x, c_count;
-    c_count = 0;
-    if(a_count < 2)
-        x = a[0];
-    else
-        x = a[1];
-    while(x <= b[0]){
-        div = 1;
-        for(j = 0 ; j < b_count ; j++)
-            if(b[j]%x != 0)
-                div = 0;
-        if(div)
-            c_count++;
-        if(x == x*a[0])
-            x++;
-        else
-            x *= a[0];
+    int i, j, c, divide, maior;
+    maior = a[0];
+    c=0;
+    for(i = 1 ;  i < a_count ; i++)
+        if(a[i]>maior)
+            maior=a[i];
+    for(i = maior ; i < b[0]+1 ; i++){
+        divide=1;
+        for(j=0 ; j < b_count ; j++){
+            //printf("%i=-=-=-=%ie%i\n", i, b[j]%i, i%a[0]);
+            if(b[j]%i!=0||i%a[0]!=0||i%maior!=0||(maior*a[0])%i==0){
+                //printf("%i-%i-%i-%i-%i\n",i,b[j],b[j]%i,i%a[0],i%a[a_count-1]);
+                divide=0;
+                break;
+            }
+        }
+        if(divide){
+            printf("%i\\\n",i);
+            c++;
+        }
     }
-
-    for(i = 1 ; i < 100 ; i++)
-        if(100%i == 0)
-            printf("%i\t%i\t", i, 100/i);
     
-    return c_count;
+    return c;
 }
 
 int main(){
-    int a[] = {2, 4};
-    int b[] = {16, 32, 96};
-    //printf("%i\n",getTotalX(2, a, 3, b));
-    int c[] = {2, 3, 6};
-    int d[] = {42, 84};
-    printf("\n%i\n",getTotalX(3, c, 2, d));
-    int e[] = {51};
-    int f[] = {50};
-    //printf("%i\n",getTotalX(1,e,1,f));
-    int g[] = {1};
-    int h[] = {72, 48};
-    //printf("%i\n", getTotalX(1,g,2,h));
+    int a[] = {3, 9, 6};
+    int b[] = {36, 72};
+    printf("%i\n",getTotalX(3, a, 2, b));
     return 0;
 }
