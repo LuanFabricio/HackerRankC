74d6921b6169f78088eac27d9990b8f778ea98cc
Seculoo <luanlimaleite@hotmail.com>
2020-07-25 19:06:24 -0300

    comeco do climbing-the-leaderboard

---
+++ climbing-the-leaderboard.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int* insertSort(int list_count, int* list, int new_num){
+	int aux[100000];
+	int i = list_count-1;
+	while(new_num>list_count[i])		
+		i--;
+	
+	for()
+}
+
+int* quickSort(int list_count, int* list){
+	int aux[100000];
+	int tamanho=1;
+	aux[0]=list[0];
+
+	// insertsort
+	for(int i = 1 ; i < list_count ; i++){
+		
+	}
+
+	return aux;
+}
+
+int* climbingLeaderboard(int scores_count, int* scores, int alice_count, int* alice, int* result_count) {
+	// for(int i = 0 ; )
+
+}
+
+int main(){
+
+	return 0;
+}
